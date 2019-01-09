e1e6451ebd6acfe29c2de89bd6f88199879cc310
Bradley Harris <bradleykh@gmail.com>
2019-01-09 10:24:45 -0600

    Initial commit

---
+++ Brisk/Models/Location.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Brisk.Models
+{
+    public class Location
+    {
+        public int Id { get; set; }
+        public int Creator { get; set; }
+        public int Zip { get; set; }
+        public string Name { get; set; }
+    }
+}
